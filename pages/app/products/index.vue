955c62258791244e45bfbba1714031718e05b8ad
ChathuraSudaraka <Chathurabro68@gmail.com>
2024-09-25 18:37:35 +0530

    Product Add and Manage !

--- pages/app/products/index.vue
+++ pages/app/products/index.vue
@@ -1,4 +1,35 @@
 <script setup lang="ts">
+const isDeleteProductModalOpen = ref(false);
+const isUpdateProductModalOpen = ref(false);
+const isInfoProductModalOpen = ref(false);
+const selected = ref<string[]>([]);
+const imagePreview = ref<string | null>(null);
+
+const handleFileChange = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  const file = target.files?.[0];
+  if (file) {
+    imagePreview.value = URL.createObjectURL(file);
+  }
+};
+
+const removeImage = () => {
+  imagePreview.value = null;
+};
+
+const category = [
+  "Wade Cooper",
+  "Arlene Mccoy",
+  "Devon Webb",
+  "Tom Cook",
+  "Tanya Fox",
+  "Hellen Schmidt",
+  "Caroline Schultz",
+  "Mason Heaney",
+  "Claudie Smitham",
+  "Emil Schaefer",
+];
+
 const columns = [
   {
     key: "id",
@@ -85,29 +116,25 @@ const products = [
 const items = (row) => [
   [
     {
-      label: "Edit",
-      icon: "i-heroicons-pencil-square-20-solid",
-      click: () => console.log("Edit", row.id),
+      label: "Update",
+      icon: "material-symbols:deployed-code-update-outline",
+      click: () => {
+        isUpdateProductModalOpen.value = true;
+      },
     },
-    {
-      label: "Duplicate",
-      icon: "i-heroicons-document-duplicate-20-solid",
-    },
-  ],
-  [
-    {
-      label: "Archive",
-      icon: "i-heroicons-archive-box-20-solid",
-    },
-    {
-      label: "Move",
-      icon: "i-heroicons-arrow-right-circle-20-solid",
-    },
-  ],
-  [
     {
       label: "Delete",
       icon: "i-heroicons-trash-20-solid",
+      click: () => {
+        isDeleteProductModalOpen.value = true;
+      },
+    },
+    {
+      label: "Info",
+      icon: "material-symbols:info",
+      click: () => {
+        isInfoProductModalOpen.value = true;
+      },
     },
   ],
 ];
@@ -130,6 +157,16 @@ const rows = computed(() => {
           Manage Products
         </h3>
         <div class="flex gap-2 justify-end mb-3">
+          <USelect
+            :options="['Mobile Phone', 'Laptop', 'Tv']"
+            size="lg"
+            placeholder="Sort by Category"
+          />
+          <USelect
+            :options="['Colombo', 'Kandy', 'Matara']"
+            size="lg"
+            placeholder="Sort by Branch"
+          />
           <UInput />
           <UButton icon="gravity-ui:magnifier" />
         </div>
@@ -174,5 +211,290 @@ const rows = computed(() => {
         </div>
       </UContent>
     </NuxtLayout>
+  </div>
+
+  <!-- Modal Here -->
+  <!-- Delete Product Here -->
+  <div>
+    <UModal v-model="isDeleteProductModalOpen" :transition="false">
+      <UCard
+        :ui="{
+          ring: '',
+        }"
+      >
+        <template #header>
+          <div class="flex items-center justify-between">
+            <h3 class="text-lg font-semibold text-red-700">Are you sure ?</h3>
+            <UButton
+              color="gray"
+              variant="ghost"
+              icon="i-heroicons-x-mark-20-solid"
+              class="-my-1"
+              @click="isDeleteProductModalOpen = false"
+            />
+          </div>
+        </template>
+
+        <div class="flex flex-col gap-3">
+          <p class="text-center">
+            This action cannot be undone. All values associated with this
+            product will be lost.
+          </p>
+        </div>
+
+        <template #footer>
+          <div class="flex justify-end gap-3">
+            <UButton
+              label="Cancel"
+              color="gray"
+              @click="isDeleteProductModalOpen = false"
+            />
+            <UButton label="Delete Product" color="red" />
+          </div>
+        </template>
+      </UCard>
+    </UModal>
+  </div>
+
+  <!-- Update Product Here -->
+  <div>
+    <UModal v-model="isUpdateProductModalOpen" :transition="false">
+      <UCard
+        :ui="{
+          ring: '',
+        }"
+      >
+        <template #header>
+          <div class="flex items-center justify-between">
+            <h3 class="text-lg font-semibold">Update Product</h3>
+            <UButton
+              color="gray"
+              variant="ghost"
+              icon="i-heroicons-x-mark-20-solid"
+              class="-my-1"
+              @click="isUpdateProductModalOpen = false"
+            />
+          </div>
+        </template>
+
+        <div class="flex flex-col gap-3">
+          <div class="grid lg:grid-cols-4 sm:grid-cols-1 gap-3">
+            <div class="grid lg:col-span-1 sm:col-span-1">
+              <h3 class="text-sm font-medium mb-1">Product Image</h3>
+              <label for="fileField" class="attachment block">
+                <div
+                  class="row btn-file border h-[193px] w-full bg-gray-200/30 dark:bg-gray-900/30 border-dashed border-gray-300 rounded-lg p-1 text-center cursor-pointer relative"
+                >
+                  <!-- Preview Area -->
+                  <div
+                    v-if="imagePreview"
+                    class="btn-file__preview h-[183px] w-full bg-center bg-cover rounded-lg"
+                    :style="{
+                      backgroundImage: 'url(' + imagePreview + ')',
+                    }"
+                  >
+                    <!-- X Button for Removing Image -->
+                    <UButton
+                      @click="removeImage"
+                      icon="material-symbols:cancel-rounded"
+                      size="sm"
+                      color="primary"
+                      square
+                      variant="solid"
+                      class="flex rounded-full"
+                    />
+                  </div>
+
+                  <div
+                    v-else
+                    class="btn-file__actions text-center flex justify-center items-center h-full"
+                  >
+                    <div class="btn-file__actions__item">
+                      <div class="btn-file__actions__item--shadow">
+                        <Icon
+                          name="material-symbols:add-photo-alternate-outline"
+                        />
+                        <h3
+                          class="text-xs uppercase text-gray-500 font-semibold mt-2"
+                        >
+                          select image
+                        </h3>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <input
+                  name="file"
+                  type="file"
+                  id="fileField"
+                  class="hidden"
+                  @change="handleFileChange"
+                />
+              </label>
+            </div>
+
+            <div class="grid lg:col-span-3 sm:col-span-1">
+              <div class="mb-3">
+                <UFormGroup label="Product Name" name="productName">
+                  <UInput placeholder="Product Name" />
+                </UFormGroup>
+              </div>
+              <div class="mb-3">
+                <UFormGroup label="Brand" name="brand">
+                  <UInput placeholder="Brand" />
+                </UFormGroup>
+              </div>
+              <div>
+                <UFormGroup label="Category" name="category">
+                  <USelectMenu
+                    v-model="selected"
+                    :options="category"
+                    multiple
+                    placeholder="Select category"
+                    size="lg"
+                    color="gray"
+                  />
+                </UFormGroup>
+              </div>
+            </div>
+          </div>
+          <div class="mt-4">
+            <h3 class="text-sm font-medium mb-1">Description</h3>
+            <UEditor />
+          </div>
+        </div>
+
+        <template #footer>
+          <div class="flex justify-end gap-3">
+            <UButton
+              label="Cancel"
+              color="gray"
+              @click="isUpdateProductModalOpen = false"
+            />
+            <UButton label="Delete " />
+          </div>
+        </template>
+      </UCard>
+    </UModal>
+  </div>
+
+  <!-- Info Product Here -->
+  <div>
+    <UModal v-model="isInfoProductModalOpen" :transition="false">
+      <UCard :ui="{ ring: '' }">
+        <template #header>
+          <div class="flex items-center justify-between">
+            <h3 class="text-lg font-semibold">Product Info</h3>
+            <UButton
+              color="gray"
+              variant="ghost"
+              icon="i-heroicons-x-mark-20-solid"
+              class="-my-1"
+              @click="isInfoProductModalOpen = false"
+            />
+          </div>
+        </template>
+
+        <!-- Product Info Section -->
+        <div class="">
+          <div class="grid grid-cols-1 lg:grid-cols-3 gap-2">
+            <!-- Average Unit Price -->
+            <div
+              class="flex flex-col items-center p-5 rounded-lg bg-gray-200/30 dark:bg-gray-900/30 border border-primary/10"
+            >
+              <div
+                class="w-10 h-10 flex justify-center items-center rounded-full bg-primary/20 text-primary mb-2"
+              >
+                <Icon
+                  name="material-symbols:stacked-line-chart"
+                  class="text-xl"
+                />
+              </div>
+              <span class="text-md font-semibold"> 132,200 </span>
+              <h3
+                class="text-xs uppercase text-center text-gray-500 font-semibold"
+              >
+                Average Unit Price
+              </h3>
+            </div>
+
+            <!-- Total Sales -->
+            <div
+              class="flex flex-col items-center p-5 rounded-lg bg-gray-200/30 dark:bg-gray-900/30 border border-primary/10"
+            >
+              <div
+                class="w-10 h-10 flex justify-center items-center rounded-full bg-primary/20 text-primary mb-2"
+              >
+                <Icon name="material-symbols:finance-mode" class="text-xl" />
+              </div>
+              <span class="text-md font-semibold">450,000</span>
+
+              <h3
+                class="text-xs uppercase text-center text-gray-500 font-semibold"
+              >
+                Total Sales
+              </h3>
+            </div>
+
+            <!-- Total Profit -->
+            <div
+              class="flex flex-col items-center p-5 rounded-lg bg-gray-200/30 dark:bg-gray-900/30 border border-primary/10"
+            >
+              <div
+                class="w-10 h-10 flex justify-center items-center rounded-full bg-primary/20 text-primary mb-2"
+              >
+                <Icon name="solar:hand-money-linear" class="text-2xl" />
+              </div>
+              <span class="text-md font-semibold"> 132,200 </span>
+              <h3
+                class="text-xs uppercase text-center text-gray-500 font-semibold"
+              >
+                Total Profit
+              </h3>
+            </div>
+          </div>
+          <div class="grid lg:col-span-3 sm:col-span-1 mt-3">
+            <div class="mb-3">
+              <UFormGroup label="Product Name" name="productName">
+                <UInput
+                  placeholder="Google Pixel 6 Pro, 8GB, 128GB, Space Gray."
+                  disabled
+                />
+              </UFormGroup>
+            </div>
+            <div class="mb-3">
+              <UFormGroup label="Brand" name="brand">
+                <UInput placeholder="Google" disabled />
+              </UFormGroup>
+            </div>
+            <div class="mb-3">
+              <UFormGroup label="Category" name="category">
+                <UInput placeholder="Mobile Phone" disabled />
+              </UFormGroup>
+            </div>
+            <div>
+              <UFormGroup label="Description" name="description">
+                <UTextarea
+                  placeholder="Description"
+                  size="lg"
+                  color="gray"
+                  disabled
+                />
+              </UFormGroup>
+            </div>
+          </div>
+        </div>
+
+        <template #footer>
+          <div class="flex justify-end gap-3">
+            <UButton
+              label="Close"
+              color="gray"
+              @click="isInfoProductModalOpen = false"
+            />
+          </div>
+        </template>
+      </UCard>
+    </UModal>
   </div>
 </template>
